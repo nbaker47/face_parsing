c66199320c38f4f97fcc000f20c0c2ef2bcded9b
nbaker47 <nathan@pop-os.localdomain>
2023-02-17 06:20:54 +0000

    improved label adapter

---
+++ research/label_test_script.py
@@ -0,0 +1,281 @@
+from PIL import ImageFilter
+from scipy import ndimage
+from collections import Counter
+import torch.nn as nn
+from torchvision import models, datasets, transforms, utils
+from torchvision.models.vgg import VGG
+from sklearn.metrics import jaccard_score, f1_score
+import tensorflow as tf
+from torchmetrics.classification import F1Score, BinaryF1Score, MulticlassF1Score, JaccardIndex
+from torchmetrics import Dice
+from sklearn.preprocessing import MultiLabelBinarizer
+from torchvision.utils import save_image
+from PIL import Image
+import torchvision.transforms as T
+import torchvision.transforms.functional as F
+import random
+#import torchvision.transforms as T
+#import torchvision.transforms.functional as F
+import albumentations as albu
+import random
+import scipy
+import torch
+import os
+import cv2
+import numpy as np
+#from google.colab.patches import cv2_imshow
+from matplotlib import pyplot as plt
+import re
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
+from PIL import Image
+import os
+import torch.nn as nn
+from torchvision import models, datasets, transforms, utils
+from torchvision.models.vgg import VGG
+
+ranges = {'vgg16': ((0, 5), (5, 10), (10, 17), (17, 24), (24, 31))}
+cfg = {'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],}
+rgb_vals = [ 0,1,2,3,4,5,6,7,8,9,10]
+DEVICE="cuda:0"
+
+
+class VGG16(VGG):
+  
+  def __init__(self, cfg, ranges, pretrained=True, model='vgg16', requires_grad=True, remove_fc=True, show_params=False):
+    super().__init__(self.make_layers(cfg[model]))
+    
+    self.ranges = ranges[model]
+        
+    if pretrained:
+      exec("self.load_state_dict(models.%s(pretrained=True).state_dict())" % model)
+    if not requires_grad:
+      for parameter in super().parameters():
+        parameter.requires_grad = False
+    if remove_fc: 
+      del self.classifier
+    if show_params:
+      for name, parameter in self.named_parameters():
+        print(name, parameter.size())
+        
+  def forward(self, x):
+    output = {}
+    
+    # Get the output of each maxpooling layer (There are 5 in VGG) to create skip connections.
+    for idx in range(len(self.ranges)):
+      for layer in range(self.ranges[idx][0], self.ranges[idx][1]):
+        x = self.features[layer](x)
+      output["pool%d"%(idx+1)] = x
+    
+    return output 
+  
+  def make_layers(self, cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+      if v == 'M':
+        layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+      else:
+        conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+        if batch_norm:
+          layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+        else:
+          layers += [conv2d, nn.ReLU(inplace=True)]
+        in_channels = v
+    return nn.Sequential(*layers)
+
+class FCN8s(nn.Module):
+  
+  def __init__(self, pretrained_model, n_classes):
+    super().__init__()
+    
+    self.n_classes = n_classes
+    self.pretrained_model = pretrained_model
+    # Transposed convolutions (to upsampling previous layers)
+    self.relu = nn.ReLU(inplace=True)
+    self.deconv1 = nn.ConvTranspose2d(512, 512, 3, 2, padding=1, dilation=1, output_padding=1)
+    self.bn1 = nn.BatchNorm2d(512)
+    self.deconv2 = nn.ConvTranspose2d(512, 256, 3, 2, padding=1, dilation=1, output_padding=1)
+    self.bn2 = nn.BatchNorm2d(256)
+    self.deconv3 = nn.ConvTranspose2d(256, 128, 3, 2, padding=1, dilation=1, output_padding=1)
+    self.bn3 = nn.BatchNorm2d(128)
+    self.deconv4 = nn.ConvTranspose2d(128,  64, 3, 2, padding=1, dilation=1, output_padding=1)
+    self.bn4 = nn.BatchNorm2d(64)
+    self.deconv5 = nn.ConvTranspose2d( 64,  32, 3, 2, padding=1, dilation=1, output_padding=1)
+    self.bn5 = nn.BatchNorm2d(32)
+    # Convolutional score layer (replace the FC layers)
+    self.score = nn.Conv2d(32, n_classes, 1)
+  
+  def forward(self, x):
+    x = self.pretrained_model(x)
+    
+    # Skip connections (connect the output of one layer to a non-adjacent layer)
+    pool_5 = x['pool5']
+    pool_4 = x['pool4']
+    pool_3 = x['pool3']
+    
+    x = self.relu(self.deconv1(pool_5))
+    x = self.bn1(x + pool_4)
+    x = self.relu(self.deconv2(x))
+    x = self.bn2(x + pool_3)
+    x = self.bn3(self.relu(self.deconv3(x)))
+    x = self.bn4(self.relu(self.deconv4(x)))
+    x = self.bn5(self.relu(self.deconv5(x)))
+    x = self.score(x)
+    
+    return x
+
+def save_predictions(model, ds):
+  #predict
+  for idx in range(len(ds)):
+
+      # Pop image from DS
+      image, gt_mask = ds[idx]
+      image_vis = image
+      image_vis = np.transpose(image_vis,(1,2,0))
+      
+      # Reshape
+      x_tensor = torch.tensor(image).to(DEVICE).unsqueeze(0)
+      # Predict test image
+      pred_mask = model(x_tensor)
+      # Reshape
+      pred_mask = pred_mask.detach().squeeze().cpu()
+      
+      # Decode Onehots
+      pred_mask = colour_code_segmentation(reverse_one_hot(torch.tensor(pred_mask)), rgb_vals)
+      gt_mask = colour_code_segmentation(reverse_one_hot(torch.tensor(gt_mask)), rgb_vals)
+
+      pred_mask = Image.fromarray(np.uint8(pred_mask)).save("/home/nathan/Documents/final_project/datasets/label_adapter_test/images/"+str(idx)+".png",quality='keep')
+      gt_mask = Image.fromarray(np.uint8(gt_mask)).save("/home/nathan/Documents/final_project/datasets/label_adapter_test/masks/"+str(idx)+".png",quality='keep')
+
+      #gt_mask = scipy.signal.medfilt(gt_mask, 9)
+
+      print("saving", idx, "/", len(ds), end="\r")
+      
+
+class MyDataSet(torch.utils.data.Dataset):
+
+  #CLASSES =  ["background","facial_skin","left_brow","right_brow","left_eye","right_eye", "nose","upper_lip","inner_mouth","lower_lip","hair"]
+
+  def __init__(self, images_dir, masks_dir, coords_dir, preprocessing=None, classes=None,augmentation=None, training=True):
+    super(MyDataSet, self).__init__()
+    
+    # store the augmented tensors tensors
+    #self._x, self._y = x,y
+    self.preprocessing = preprocessing
+    self.augmentation = augmentation
+
+    self.image_ids = os.listdir(images_dir)
+
+    self.images_fps = [os.path.join(images_dir, image_id) for image_id in self.image_ids]
+    self.masks_fps = [os.path.join(masks_dir, mask_id) for mask_id in self.image_ids]
+
+  def __len__(self):
+    # a DataSet must know it size
+    return len(self.images_fps)
+
+  def __getitem__(self, i):
+
+    image = cv2.imread(self.images_fps[i], 0)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    mask = cv2.imread(self.masks_fps[i], cv2.IMREAD_GRAYSCALE)
+
+    image = torch.tensor(image)
+
+    #onehot
+    one_hot_Y = torch.nn.functional.one_hot(torch.tensor(mask).to(torch.int64), 11).float()
+
+    return (image.permute(2,0,1).float(), one_hot_Y.permute(2,0,1).float())
+
+# helper function for data visualization
+def visualize(**images):
+    """
+    Plot images in one row (Original Image: Ground Truth : Predicted)
+    """
+    n_images = len(images)
+    plt.figure(figsize=(20,8))
+    for idx, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n_images, idx + 1)
+        plt.xticks([]); 
+        plt.yticks([])
+        if name == "original_image":
+          #print(image.shape)
+          plt.title(name.replace('_',' ').title(), fontsize=20)
+          plt.imshow(image)
+        else:
+          #print(image.shape)
+          # get title from the parameter names
+          plt.title(name.replace('_',' ').title(), fontsize=20)
+          plt.imshow(image)
+    plt.show()
+
+#Function to reverse one-hot-encode an image
+def reverse_one_hot(image):
+    #argmax to return the indices of the maximum values along an axis. 
+    x = np.argmax(image, axis = -3)
+    return x
+
+# Perform colour coding on the reverse-one-hot outputs
+def colour_code_segmentation(image, label_values):
+    colour_codes = np.array(label_values)
+    x = colour_codes[image.numpy().astype(int)]
+
+    return x
+
+def average(lst):
+    return sum(lst) / len(lst)
+
+def view_label_predictions(model, input_ds, num_classes ):
+    # save model predictions
+    save_predictions(model, input_ds)
+
+    """DS"""
+    train_x_path = "/home/nathan/Documents/final_project/datasets/label_adapter_test/images"
+    train_y_path = "/home/nathan/Documents/final_project/datasets/label_adapter_test/masks"
+    ds = MyDataSet(train_x_path, train_y_path, None )
+
+    fcn = FCN8s(VGG16(cfg,ranges), num_classes)
+    fcn.to(DEVICE)
+    model = fcn
+    model.load_state_dict(torch.load('/home/nathan/Documents/final_project/saved_models/label_adapted.pth'))
+
+    f1s = []
+    #predict
+    for idx in range(len(ds)):
+        image, gt_mask = ds[idx]
+        image = image.cpu()
+        image_vis = image.cpu()
+        image_vis = np.transpose(image_vis,(1,2,0))
+        x_tensor = torch.tensor(image).to(DEVICE).unsqueeze(0)
+        pred_mask = model(x_tensor)
+        pred_mask = pred_mask.detach().squeeze().cpu().numpy()
+        pred_mask = colour_code_segmentation(reverse_one_hot(torch.tensor(pred_mask)), rgb_vals)
+        gt_mask = colour_code_segmentation(reverse_one_hot(torch.tensor(gt_mask)), rgb_vals)
+        metric = MulticlassF1Score(num_classes=num_classes, average=None, labels=np.unique(pred_mask) ,validate_args=True)
+        f1 = metric(torch.tensor(pred_mask), torch.tensor(gt_mask))
+        if len(f1) == num_classes:
+          f1[f1 <0.1] = np.nan
+          f1s.append(np.array(f1))
+        try:
+          if idx < 1:
+            visualize(
+                original_image = image[0,::],
+                ground_truth_mask = gt_mask,
+                predicted_mask = pred_mask,
+            )
+        except:
+          if idx < 1:
+            visualize(
+                original_image = image.cuda()[0,::],
+                ground_truth_mask = gt_mask.cuda(),
+                predicted_mask = pred_mask.cuda(),
+            )
+    
+    fs1_numpy = np.array(f1s)
+    av_f1s = np.nanmean(fs1_numpy, axis=0)
+    #av_f1s = fs1_numpy.mean(axis=0)
+    av_f1s_av = av_f1s.mean(axis=0)
+
+    #print ("Dataset MIoU = ", average(ious))
+    print ("Dataset F1 = ", av_f1s)
+    print ("Dataset F1 av = ", av_f1s_av)
